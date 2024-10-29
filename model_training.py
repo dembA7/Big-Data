2bb7e3b1e56be7fed3aefa03c9a1c560b1e26e3d
dembA7 <a01709522@tec.mx>
2024-10-28 23:05:36 -0600

    add model_training.py

---
+++ model_training.py
@@ -0,0 +1,21 @@
+from pyspark.sql import SparkSession
+from pyspark.ml.classification import RandomForestClassifier
+from pyspark.ml.evaluation import MulticlassClassificationEvaluator
+from pyspark.sql import DataFrameReader
+
+# Creo mi sesión de Spark
+spark = SparkSession.builder \
+    .appName("Fashion MNIST Model Training") \
+    .config("spark.executor.memory", "10g") \
+    .config("spark.driver.memory", "5g") \
+    .getOrCreate()
+
+# Cargo los datos preparados
+train_data = spark.read.parquet("data/prepared_train_data.parquet").repartition(100)
+
+# Entreno el modelo
+rf = RandomForestClassifier(featuresCol='features', labelCol='label', numTrees=100)
+model = rf.fit(train_data)
+
+# Guardo el modelo
+model.save("models/fashion_mnist_rf_model")
